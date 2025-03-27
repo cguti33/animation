6f9dcb524e3e5affc201f352a6a21a182de57a2e
cguti33 <cguti33@lsu.edu>
2025-03-27 10:05:34 -0500

    text bubble

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // JS scripts placed here
+const trigger = document.querySelector('.fade-element');
+const menu = document.getElementById('offcanvas-menu');
+document.body.addEventListener('click', () => {
+    console.log('clicked');
+//   const expanded = trigger.getAttribute('aria-expanded') === 'true' || false;
+//   trigger.setAttribute('aria-expanded',!expanded);
+  trigger.classList.toggle('visible');
+});
